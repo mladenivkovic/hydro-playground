d71bc8a238782afcaecf39d44b599d27f00cf39a
sean baccas <sean.d.baccas@durham.ac.uk>
2024-01-07 15:22:55 +0000

    change the parameters class to contain a single static copy of itself to hold global variables

--- src/Parameters.h
+++ src/Parameters.h
@@ -1,5 +1,23 @@
 #pragma once
 
+/*
+
+Turning this class into singleton pattern. In any file where you 
+include Parameters.h, you can call 
+hydro_playground::parameters::Parameters::Instance._nstepsLog (for example)
+and it will be this single global copy. 
+
+Since the member variables are nonstatic now i've un-deleted the default 
+constructor. This will be called by default on the static member anyhow
+
+We could remove the namespaceing
+here as it is a bit of a mouthful to type... 
+
+It's up to us whether we make the instance itself private and use the 
+getter or just make it public. It doesn't make a difference, since we
+need to return a reference anyway...
+
+*/
 
 namespace hydro_playground {
   namespace parameters {
@@ -14,35 +32,35 @@ namespace hydro_playground {
       // int _verbose;
 
       //! interval between steps to write current state to screen
-      static int _nstepsLog;
+      int _nstepsLog;
 
 
       // simulation related parameters
       // -----------------------------
 
       //! How many steps to do
-      static int _nsteps;
+      int _nsteps;
 
       //! at what time to end simulation
-      static float _tmax;
+      float _tmax;
 
       //! number of cells to use (in each dimension)
-      static int _nx;
+      int _nx;
 
       //! CFL coefficient
-      static float _ccfl;
+      float _ccfl;
 
       //! time step sized used when enforcing a fixed time step size
       // float _force_dt;
 
       //! boundary condition
-      static int _boundary;
+      int _boundary;
 
       //! number of mesh points, including boundary cells
-      static int _nxTot;
+      int _nxTot;
 
       //! cell size
-      static float _dx;
+      float _dx;
 
 
       // Output related parameters
@@ -109,35 +127,43 @@ namespace hydro_playground {
 
 
     public:
-      Parameters() = delete;
+      Parameters();
 
-      static void init();
+      void init();
 
-      static void cleanup();
+      void cleanup();
 
-      static int  getNstepsLog();
-      static void setNstepsLog(const int nsteps_log);
+      int  getNstepsLog();
+      void setNstepsLog(const int nsteps_log);
 
-      static int  getNsteps();
-      static void setNsteps(const int nsteps);
+      int  getNsteps();
+      void setNsteps(const int nsteps);
 
-      static float getTmax();
-      static void  setTmax(const float tmax);
+      float getTmax();
+      void  setTmax(const float tmax);
 
-      static int  getNx();
-      static void setNx(const int nx);
+      int  getNx();
+      void setNx(const int nx);
 
-      static float getCcfl();
-      static void  setCcfl(float ccfl);
+      float getCcfl();
+      void  setCcfl(float ccfl);
 
-      static int  getBoundary();
-      static void setBoundary(const int boundary);
+      int  getBoundary();
+      void setBoundary(const int boundary);
 
-      static int  getNxTot();
-      static void setNxTot(const int nxTot);
+      int  getNxTot();
+      void setNxTot(const int nxTot);
 
-      static float getDx();
-      static void  setDx(const float dx);
+      float getDx();
+      void  setDx(const float dx);
+    
+    public: 
+
+      // single copy of the global variables
+      static Parameters Instance;
+
+      // getter for the single global copy
+      static Parameters& getInstance() {return Instance;}
     };
 
 
