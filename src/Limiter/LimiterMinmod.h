90b7cab4b4c32bb5508b6b14ee24f294df74f34a
Mladen Ivkovic <mivkov@protonmail.com>
2025-03-17 15:54:07 +0000

    fixed bug in minmod

--- src/Limiter/LimiterMinmod.h
+++ src/Limiter/LimiterMinmod.h
@@ -11,16 +11,18 @@ namespace limiter {
 
   /**
    * Compute the actual slope limiter xi(r) for the minmod limiter
+   * Eq. 104 in theory document.
    */
   inline Float limiterXiOfR(const Float r){
 
     Float xi = r > 0. ? r : 0.;
     if (r > 1.) {
+      // Eq. 106
       Float d   = 1. - cst::MUSCL_SLOPE_OMEGA + (1. + cst::MUSCL_SLOPE_OMEGA) * r;
       Float xiR = 2. / d;
 
       // xi = min(1, xiR)
-      xi = (xiR < xi) ? xiR : 1.;
+      xi = (xiR < 1.) ? xiR : 1.;
     }
     return xi;
   }
