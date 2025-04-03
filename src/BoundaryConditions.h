3880eea230ef9c896418dbd89d48de4fd53ebb35
Mladen Ivkovic <mivkov@protonmail.com>
2025-04-03 15:56:58 +0100

    removed vectors in boundary conditions

--- src/BoundaryConditions.h
+++ src/BoundaryConditions.h
@@ -5,6 +5,13 @@
 
 #pragma once
 
+#include <cstddef>
+#include <utility>
+#include <string>
+
+#include "Cell.h"
+
+
 namespace BC {
 
   //! Boundary condition types
@@ -36,3 +43,65 @@ namespace BC {
   }
 
 } // namespace BC
+
+
+/**
+ * A simple container for boundary cells. You can in principle just use a list
+ * or a vector, but those STL objects are not trivially copyable - something
+ * e.g. openMP offloading is not happy about. So I write proper constructors
+ * by myself.
+ */
+class Boundary{
+
+public:
+
+  size_t _n;
+  Cell** _cells;
+
+  explicit Boundary(const size_t N) : _n(N), _cells(new Cell*[N]) {};
+
+  ~Boundary() {
+    delete[] _cells;
+  }
+
+  // Copy operator
+  Boundary(const Boundary& other) : _n(other._n) {
+    for (size_t i = 0; i < _n; i++)
+      _cells[i] = other._cells[i];
+  }
+
+  // Copy assignment
+  Boundary& operator=(const Boundary& other) {
+    this->_n= other._n;
+    for (size_t i = 0; i < _n; i++)
+      this->_cells[i] = other._cells[i];
+    return *this;
+  }
+
+  // Move operator
+  Boundary(const Boundary&& other)  noexcept : _n(other._n) {
+    _cells = std::move(other._cells);
+  }
+
+  // Move assignment
+  Boundary& operator=(Boundary&& other) noexcept {
+    this->_n= other._n;
+    this->_cells = other._cells;
+    return *this;
+  }
+
+  // overload [] operator
+  Cell*& operator[](size_t index){
+#if DEBUG_LEVEL > 0
+    if (index > _n)
+      error("Invalid index:" + std::to_string(index));
+#endif
+    return _cells[index];
+  }
+
+  size_t size(){
+    return _n;
+  }
+};
+
+
